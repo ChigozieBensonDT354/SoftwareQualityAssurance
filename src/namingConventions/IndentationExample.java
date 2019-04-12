48c057f1d9c14f48d5991daa1dc9a865affc98dc
Chigozie Benson <chigozie.benson@sap.com>
2019-04-12 19:24:37 +0100

    add

--- src/namingConventions/IndentationExample.java
+++ src/namingConventions/IndentationExample.java
@@ -8,7 +8,7 @@ package namingConventions;
 public class IndentationExample {
     //Indented 4 spaces in
     public IndentationExample() {
-	    // TODO Auto-generated constructor stub
+	    // TODO Auto-generated constructor stub 
 	}
 
 }
